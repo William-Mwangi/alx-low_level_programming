da83dddea75c585c43f7f35ff448d08fa9c861bb
William-Mwangi <reginaldwilliam90@gmail.com>
2023-09-08 02:55:42 -0700

    a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-/*
+/**
  * main - get sizes of data types
- * return - returns zero
+ * Return: returns zero
  */
 int main(void)
 {
 int intType;
 float floatType;
-double doubleType;
 char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
 
