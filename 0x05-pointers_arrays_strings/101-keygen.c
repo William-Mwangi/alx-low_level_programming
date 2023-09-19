652817ce93752a2547972699c9a42c1fb02980fb
William-Mwangi <reginaldwilliam90@gmail.com>
2023-09-19 15:25:04 -0700

    a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,51 +3,34 @@
 #include <time.h>
 
 /**
- * generatePassword - Function to generate a random password
- * @password: pointer
- * @length: length
- * Return: nothing
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
 
-void generatePassword(char *password, int length)
+int main(void)
 {
-	char chst[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	int charsetSize = sizeof(chst) - 1;
+	int pass[100];
+	int i, sum, n;
 
-	for (int i = 0; i < length; i++)
-	{
-		int randomIndex = rand() % charsetSize;
-
-		password[i] = chst[randomIndex];
-	}
-	password[length] = '\0';
-}
-
-/**
- * main - check code
- * @argc: counter
- * @argv: commandline argment
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <password_length>\n", argv[0]);
-		return (1);
-	}
-	int passwordLength = atoi(argv[1]);
-
-	if (passwordLength <= 0)
-	{
-		fprintf(stderr, "Invalid password length\n");
-		return (1);
-	}
+	sum = 0;
 	srand(time(NULL));
 
-	char password[passwordLength + 1];
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-	generatePassword(password, passwordLength);
-	printf("Generated Password: %s\n", password);
 	return (0);
 }
+
