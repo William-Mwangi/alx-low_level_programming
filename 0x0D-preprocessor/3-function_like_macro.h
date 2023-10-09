1e7150994a251fae64d85543c4b4315ec2112070
William-Mwangi <reginaldwilliam90@gmail.com>
2023-10-09 12:58:08 -0700

    hot fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef _3_function_like_macro_h_
 #define _3_function_like_macro_h_
-#include <stdlib.h>
 
-#define ABS(x) ((x) < 0 ? -(x) : abs(x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
