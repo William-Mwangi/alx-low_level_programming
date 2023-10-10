b4a714f576dd32080c16057d076ced90d6a820fd
William-Mwangi <reginaldwilliam90@gmail.com>
2023-10-10 03:49:24 -0700

    hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,16 +17,12 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)");
 		}
-		if (d->age < 0)
-		{
-			printf("Age: %.6f", -1.0);
-		}
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)");
 		}
 		printf("Name: %s\n", d->name);
-		printf("Age: %.6f\n", d->age);
+		printf("Age: %.6f\n", (d->age >= 0) ? d->age : -1.0);
 		printf("Owner: %s\n", d->owner);
 	}
 }
